f9bc396b348e824886dd4203bb790e52cc6c107c
SMART24\User <kny448600@gmail.com>
2025-03-31 15:15:18 +0900

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:untitled/button_page.dart';
 import 'package:untitled/flex_file.dart';
 import 'package:untitled/image_page.dart';
+import 'package:untitled/input_page.dart';
 import 'package:untitled/popup_page.dart';
 import 'package:untitled/SETTING_PAGE.dart';
 
@@ -14,6 +15,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   // This widget is the root of your application.
   @override
@@ -31,6 +33,7 @@ class MyApp extends StatelessWidget {
         '/flex': (context) => const FlexPage(),
         '/image': (context) => const ImagePage(),
         '/button': (context) => const ButtonPage(),
+        '/input': (context) => const InputPage(),
       },
     );
   }
